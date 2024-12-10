9b66204b657e96743f43213399f53bf1dd74f082
Ar-Akhil <ar.akhil1511@gmail.com>
2024-12-10 22:03:09 +0530

    Fix : added difference between production and development

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -12,7 +12,10 @@ const sendToken = (user, statusCode, res) => {
     secure: process.env.NODE_ENV === "PRODUCTION",
     httpOnly: true,
     sameSite: process.env.NODE_ENV === "PRODUCTION" ? "None" : "Lax",
-    domain: process.env.NODE_ENV === "PRODUCTION" ? ".onrender.com" : undefined,
+    domain:
+      process.env.NODE_ENV === "PRODUCTION"
+        ? "buynow-66f3.onrender.com"
+        : undefined,
   };
   res.status(statusCode).cookie("token", token, options).json({
     success: true,
