df44b162138a244461b7b9f29845032af63ce288
Ar-Akhil <ar.akhil1511@gmail.com>
2025-01-09 21:13:11 +0530

    Fix : Added username to order and fixed few typos

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -6,191 +6,192 @@ const stripe = require("stripe")(process.env.STRIPE_API_KEY);
 
 // Stripe Checkout => /api/v1/checkout
 exports.checkOut = catchAsyncErrors(async (req, res, next) => {
-  const line_items = req.body.line_items.map((item) => ({
-    price_data: {
-      currency: "usd",
-      product_data: {
-        name: item.product_name,
-      },
-      unit_amount: item.price * 100,
-    },
-    quantity: item.quantity,
-  }));
-
-  const taxAmount = req.body.tax.amount;
-  const shippingAmount = req.body.shipping.shippingAmount;
-
-  try {
-    const session = await stripe.checkout.sessions.create({
-      line_items: [
-        ...line_items,
-        {
-          price_data: {
+    const line_items = req.body.line_items.map((item) => ({
+        price_data: {
             currency: "usd",
             product_data: {
-              name: "Tax",
+                name: item.product_name,
             },
-            unit_amount: taxAmount * 100,
-          },
-          quantity: 1,
+            unit_amount: item.price * 100,
         },
-      ],
-      mode: "payment",
-      success_url:
-        process.env.NODE_ENV === "PRODUCTION"
-          ? "https://buynow-66f3.onrender.com/success"
-          : "http://localhost:5173/success",
-      cancel_url:
-        process.env.NODE_ENV === "PRODUCTION"
-          ? "https://buynow-66f3.onrender.com"
-          : "http://localhost:5173",
+        quantity: item.quantity,
+    }));
 
-      shipping_options: [
-        {
-          shipping_rate_data: {
-            type: "fixed_amount",
-            fixed_amount: {
-              amount: shippingAmount * 100,
-              currency: "usd",
-            },
-            display_name: "Standard Shipping",
-            delivery_estimate: {
-              minimum: {
-                unit: "business_day",
-                value: 5,
-              },
-              maximum: {
-                unit: "business_day",
-                value: 7,
-              },
-            },
-          },
-        },
-      ],
-    });
-    res.status(201).json({
-      success: true,
-      sessionId: session.id,
-      paymentUrl: session.url,
-    });
-  } catch (error) {
-    console.error("Stripe Error:", error);
-    next(error);
-  }
+    const taxAmount = req.body.tax.amount;
+    const shippingAmount = req.body.shipping.shippingAmount;
+
+    try {
+        const session = await stripe.checkout.sessions.create({
+            line_items: [
+                ...line_items,
+                {
+                    price_data: {
+                        currency: "usd",
+                        product_data: {
+                            name: "Tax",
+                        },
+                        unit_amount: taxAmount * 100,
+                    },
+                    quantity: 1,
+                },
+            ],
+            mode: "payment",
+            success_url:
+                process.env.NODE_ENV === "PRODUCTION"
+                    ? "https://buynow-66f3.onrender.com/success"
+                    : "http://localhost:5173/success",
+            cancel_url:
+                process.env.NODE_ENV === "PRODUCTION"
+                    ? "https://buynow-66f3.onrender.com"
+                    : "http://localhost:5173",
+
+            shipping_options: [
+                {
+                    shipping_rate_data: {
+                        type: "fixed_amount",
+                        fixed_amount: {
+                            amount: shippingAmount * 100,
+                            currency: "usd",
+                        },
+                        display_name: "Standard Shipping",
+                        delivery_estimate: {
+                            minimum: {
+                                unit: "business_day",
+                                value: 5,
+                            },
+                            maximum: {
+                                unit: "business_day",
+                                value: 7,
+                            },
+                        },
+                    },
+                },
+            ],
+        });
+        res.status(201).json({
+            success: true,
+            sessionId: session.id,
+            paymentUrl: session.url,
+        });
+    } catch (error) {
+        console.error("Stripe Error:", error);
+        next(error);
+    }
 });
 
 //Create new order => /api/v1/order/new
-exports.newOrder = catchAsyncErrors(async (req, res, next) => {
-  const {
-    shippingInfo,
-    orderItems,
-    paymentInfo,
-    itemsPrice,
-    taxPrice,
-    shippingPrice,
-    totalPrice,
-  } = req.body;
-  const order = await Order.create({
-    shippingInfo,
-    orderItems,
-    paymentInfo,
-    itemsPrice,
-    taxPrice,
-    shippingPrice,
-    totalPrice,
-    paidAt: Date.now(),
-    user: req.user._id,
-  });
-  res.status(201).json({
-    success: true,
-    order,
-  });
+exports.newOrder = catchAsyncErrors(async (req, res) => {
+    const {
+        shippingInfo,
+        orderItems,
+        paymentInfo,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+    } = req.body;
+    const order = await Order.create({
+        shippingInfo,
+        orderItems,
+        paymentInfo,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+        paidAt: Date.now(),
+        user: req.user._id,
+        userName: req.user.name,
+    });
+    res.status(201).json({
+        success: true,
+        order,
+    });
 });
 
 //Get single order => /api/v1/order/:id
 
 exports.getSingleOrder = catchAsyncErrors(async (req, res, next) => {
-  const order = await Order.findById(req.params.id).populate(
-    "user",
-    "name email"
-  );
+    const order = await Order.findById(req.params.id).populate(
+        "user",
+        "name email"
+    );
 
-  if (!order) {
-    return next(new ErrorHandler("Order not found", 404));
-  }
-  res.status(200).json({
-    success: true,
-    order,
-  });
+    if (!order) {
+        return next(new ErrorHandler("Order not found", 404));
+    }
+    res.status(200).json({
+        success: true,
+        order,
+    });
 });
 
 //Get loggedin user orders   => /api/v1/orders/me
 
-exports.myOrder = catchAsyncErrors(async (req, res, next) => {
-  const orders = await Order.find({
-    user: req.user.id,
-  });
-  res.status(200).json({
-    success: true,
-    noOfOrders: orders.length,
-    orders,
-  });
+exports.myOrder = catchAsyncErrors(async (req, res) => {
+    const orders = await Order.find({
+        user: req.user.id,
+    });
+    res.status(200).json({
+        success: true,
+        noOfOrders: orders.length,
+        orders,
+    });
 });
 
 //Get all orders   => /api/v1/admin/orders/
 
-exports.allOrders = catchAsyncErrors(async (req, res, next) => {
-  const orders = await Order.find();
-  let totalAmount = 0;
-  orders.forEach((order) => {
-    totalAmount += order.totalPrice;
-  });
-  res.status(200).json({
-    success: true,
-    totalAmount,
-    orders,
-  });
+exports.allOrders = catchAsyncErrors(async (req, res) => {
+    const orders = await Order.find();
+    let totalAmount = 0;
+    orders.forEach((order) => {
+        totalAmount += order.totalPrice;
+    });
+    res.status(200).json({
+        success: true,
+        totalAmount,
+        orders,
+    });
 });
 
 //Update / Process order - ADMIN  => /api/v1/admin/order/:id
 
 exports.updateOrders = catchAsyncErrors(async (req, res, next) => {
-  const order = await Order.findById(req.params.id);
-  if (order.orderStatus === "Delivered") {
-    return next(new ErrorHandler("Order already delivered", 400));
-  }
-  order.orderItems.forEach(async (item) => {
-    await updateStock(item.product, item.quantity);
-  });
+    const order = await Order.findById(req.params.id);
+    if (order.orderStatus === "Delivered") {
+        return next(new ErrorHandler("Order already delivered", 400));
+    }
+    for (const item of order.orderItems) {
+        await updateStock(item.product, item.quantity);
+    }
 
-  (order.orderStatus = req.body.status),
-    (order.deliveredAt = Date.now()),
+    order.orderStatus = req.body.status
+    order.deliveredAt = Date.now()
     await order.save();
-  res.status(200).json({
-    success: true,
-    order,
-  });
+    res.status(200).json({
+        success: true,
+        order,
+    });
 });
 
 async function updateStock(id, quantity) {
-  const product = await Product.findById(id);
-  product.stock = product.stock - quantity;
-  await product.save({
-    validateBeforeSave: false,
-  });
+    const product = await Product.findById(id);
+    product.stock = product.stock - quantity;
+    await product.save({
+        validateBeforeSave: false,
+    });
 }
 
 //delete order => /api/v1/admin/order/:id
 
 exports.deleteOrder = catchAsyncErrors(async (req, res, next) => {
-  const order = await Order.findById(req.params.id);
+    const order = await Order.findById(req.params.id);
 
-  if (!order) {
-    return next(new ErrorHandler("Order not found", 404));
-  }
-  await Order.deleteOne();
+    if (!order) {
+        return next(new ErrorHandler("Order not found", 404));
+    }
+    await Order.deleteOne();
 
-  res.status(200).json({
-    success: true,
-  });
+    res.status(200).json({
+        success: true,
+    });
 });
--- models/order.js
+++ models/order.js
@@ -1,102 +1,106 @@
 const mongoose = require("mongoose");
 const orderSchema = mongoose.Schema({
-  shippingInfo: {
-    address: {
-      type: String,
-      required: true,
+    shippingInfo: {
+        address: {
+            type: String,
+            required: true,
+        },
+        city: {
+            type: String,
+            required: true,
+        },
+        phoneNo: {
+            type: String,
+            required: true,
+        },
+        postalCode: {
+            type: String,
+            required: true,
+        },
+        country: {
+            type: String,
+            required: true,
+        },
     },
-    city: {
-      type: String,
-      required: true,
-    },
-    phoneNo: {
-      type: String,
-      required: true,
-    },
-    postalCode: {
-      type: String,
-      required: true,
-    },
-    country: {
-      type: String,
-      required: true,
-    },
-  },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  orderItems: [
-    {
-      name: {
-        type: String,
-        required: true,
-      },
-      quantity: {
-        type: Number,
-        required: true,
-      },
-      image: {
-        type: String,
-        required: true,
-      },
-      price: {
-        type: Number,
-        required: true,
-      },
-      product: {
+    user: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "Products",
+        ref: "User",
         required: true,
-      },
     },
-  ],
-  paymentInfo: {
-    id: {
-      type: String,
-      required: true,
+    userName: {
+        type: String,
+        required: true,
     },
-    status: {
-      type: String,
-      required: true,
+    orderItems: [
+        {
+            name: {
+                type: String,
+                required: true,
+            },
+            quantity: {
+                type: Number,
+                required: true,
+            },
+            image: {
+                type: String,
+                required: true,
+            },
+            price: {
+                type: Number,
+                required: true,
+            },
+            product: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Products",
+                required: true,
+            },
+        },
+    ],
+    paymentInfo: {
+        id: {
+            type: String,
+            required: true,
+        },
+        status: {
+            type: String,
+            required: true,
+        },
     },
-  },
-  paidAt: {
-    type: Date,
-  },
-  itemsPrice: {
-    type: Number,
-    required: true,
-    default: 0.0,
-  },
-  taxPrice: {
-    type: Number,
-    required: true,
-    default: 0.0,
-  },
-  shippingPrice: {
-    type: Number,
-    required: true,
-    default: 0.0,
-  },
-  totalPrice: {
-    type: Number,
-    required: true,
-    default: 0.0,
-  },
-  orderStatus: {
-    type: String,
-    required: true,
-    default: "Order Confirmed",
-  },
-  deliveredAt: {
-    type: Date,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+    paidAt: {
+        type: Date,
+    },
+    itemsPrice: {
+        type: Number,
+        required: true,
+        default: 0.0,
+    },
+    taxPrice: {
+        type: Number,
+        required: true,
+        default: 0.0,
+    },
+    shippingPrice: {
+        type: Number,
+        required: true,
+        default: 0.0,
+    },
+    totalPrice: {
+        type: Number,
+        required: true,
+        default: 0.0,
+    },
+    orderStatus: {
+        type: String,
+        required: true,
+        default: "Order Confirmed",
+    },
+    deliveredAt: {
+        type: Date,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
 });
 
 module.exports = mongoose.model("Order", orderSchema);
